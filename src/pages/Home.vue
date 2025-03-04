d198902583d75ba344eeee458b8daeb3cd055c3b
RNB-mac-mini <rodrigonbertin@gmail.com>
2025-03-04 18:04:43 -0300

    template base

---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import useTestStore from '../store/test'
+const testStore = useTestStore()
+</script>
+
+<template>
+  <h2>{{testStore.data}} Home Page!!!</h2>
+</template>
+
---
+++ src/router.ts
@@ -0,0 +1,19 @@
+import {createMemoryHistory, createRouter as _createRouter, createWebHistory, RouteRecordRaw,} from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('./pages/Home.vue'),
+    meta: {
+      pageName: 'Home',
+    },
+  },
+];
+
+export function createRouter() {
+  return _createRouter({
+    history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
+    routes,
+  });
+}
