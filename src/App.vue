9032cd666c72f063ef0ce45f418cda6ffe9546e4
RNB-mac-mini <rodrigonbertin@gmail.com>
2025-03-04 19:12:38 -0300

    Integrando tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container">
-    <h1>Hello to Vue 3 (SSR)</h1>
+  <div class="container flex flex-col align-middle items-center mt-4">
+    <h1 class="text-3xl mt-5 mb-3 text-cyan-800 font-bold">Hello to Vue 3 (SSR)</h1>
     <router-view></router-view>
   </div>
 </template>
